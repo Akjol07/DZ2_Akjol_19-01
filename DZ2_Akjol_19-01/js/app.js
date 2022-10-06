ac92e62e4973b85a8ff8c834bf9e18c7464bd88f
Travis <115097456+Akjol07@users.noreply.github.com>
2022-10-06 21:16:57 +0600

    Add files via upload

---
+++ DZ2_Akjol_19-01/js/app.js
@@ -0,0 +1,20 @@
+// Цикл
+let i = 0;
+while (i < 51) {
+    alert(i);
+    console.log(i);
+    i++;
+}
+
+// Объекты
+let user = {
+    name: "Akjol",
+    age: 18,
+    address: "California"
+}
+console.log(user);
+
+// Массивы
+let arr = ["Привет","Мир"+"!"];
+alert( arr );
+console.log(arr);
